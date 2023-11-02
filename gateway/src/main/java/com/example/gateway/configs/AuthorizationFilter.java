9fb66074a5c7b409f895e77e008e80425d0a6360
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-11-02 16:06:09 +0300

    Update AuthorizationFilter.java
    Redundant methods have been changed.

--- gateway/src/main/java/com/example/gateway/configs/AuthorizationFilter.java
+++ gateway/src/main/java/com/example/gateway/configs/AuthorizationFilter.java
@@ -1,6 +1,6 @@
 package com.example.gateway.configs;
 
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.http.HttpHeaders;
@@ -12,18 +12,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-import java.util.Map;
-
 @Component
 public class AuthorizationFilter implements GatewayFilter {
     private final RestTemplate template;
 
-
-    private final Map<String, List<String>> allowedRolesPathMapping; // Yollar ve izin verilen rollerin haritası
-
-    public AuthorizationFilter(Map<String, List<String>> allowedRolesPathMapping, RestTemplate template) {
-        this.allowedRolesPathMapping = allowedRolesPathMapping;
+    public AuthorizationFilter(RestTemplate template) {
         this.template = template;
     }
 
@@ -31,38 +24,27 @@ public class AuthorizationFilter implements GatewayFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
 
-        // Authorization header'dan tokeni çıkarın
         String authorizationHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
         String token = authorizationHeader.substring("Bearer ".length());
 
         String role = validateToken(token).getBody();
 
-        // İstenen yolu alın
         String requestPath = request.getPath().toString();
-        String path= pathSplit(requestPath,3);
-        // İstenilen yol için rolün izin verilip verilmediğini kontrol edin
-        List<String> allowedRoles = allowedRolesPathMapping.get(path);
-        // allowedRoles bir List<String> türünde ise
-        List<String> allowedRolesList = allowedRolesPathMapping.get(path);
-
-        // role bir tek String olarak "ROLE_USER,ROLE_ADMIN" gibi bir değeri temsil ediyorsa
-        String[] userRoles = role.split(",");
-
-        boolean isAuthorized = false;
-
-        for (String userRole : userRoles) {
-            if (allowedRolesList.contains(userRole)) {
-                isAuthorized = true;
-                break;
+        if (requestPath.startsWith("/v1/doc")) {
+            if (!role.contains("ROLE_ADMIN")) {
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
             }
-        }
-
-        if (!isAuthorized) {
+        } else if (requestPath.startsWith("/v1/auth")) {
+            if (!role.contains("ROLE_USER")) {
+                exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
+                return exchange.getResponse().setComplete();
+            }
+        } else {
             exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
             return exchange.getResponse().setComplete();
         }
 
-        // Rol izinlidir, istekle devam edin
         return chain.filter(exchange);
     }
 
@@ -70,17 +52,6 @@ public class AuthorizationFilter implements GatewayFilter {
         return template.getForEntity("http://localhost:9001/v1/auth/validate?token=" + token, String.class);
     }
 
-    private String pathSplit(String path, int splitNumber){
-// URL'den sadece "/v1/doc" kısmını almak
-        String[] parts = path.split("/"); // URL'yi "/" karakterine göre böler
-
-        if (parts.length >= splitNumber) {
-            String desiredPart = "/" + parts[1] + "/" + parts[2];
-            return desiredPart;
-        } else {
-            throw new RuntimeException();
-        }
-    }
 }
 
 
